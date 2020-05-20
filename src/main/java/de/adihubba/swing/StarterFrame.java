977e1807b145ec218abb15e2c7ef77cabd1dbf61
TimofeyKochetkov <ktmm1@yandex.ru>
2020-05-20 22:06:06 +0300

    Reworked scaling of the parsing event, fixed initial mistakes and bugs, tested.

--- src/main/java/de/adihubba/swing/StarterFrame.java
+++ src/main/java/de/adihubba/swing/StarterFrame.java
@@ -25,17 +25,18 @@ public class StarterFrame extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
+    //EDITED BY Timofey Kochetkov https://github.com/TimofeyKochetkov/javafx-3d-surface-chart
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double time;
-        System.out.println("Введите значение параметра времени для графика");
+        System.out.println("Enter the value of TIME parameter");
         time = Double.parseDouble(sc.nextLine());
         System.out.printf("Time == %f \n", time);
         System.out
-                .println("Введите адрес папки с файлами, содержащими координаты, или оставьте следующую строку пустой");
+                .println("Enter the path to the folder containing formatted files with coordinates");
         File[] files = new File(sc.nextLine()).listFiles();
-        if (files == null) System.out.println("Данные о файлах не были введены");
+        if (files == null) System.out.println("No files were added to the plotting");
         else {
             int c = 1;
             for (File file : files) {
@@ -43,7 +44,7 @@ public class StarterFrame extends JFrame {
                     final_Points.addAll(getPointsFromFile(file));
                     System.out.println(file.toString() + " --- File No. " + c++);
                 } catch (IOException e) {
-                    System.out.println("Ошибка с файлом");
+                    System.out.println("Error with parsing the file");
                 }
             }
         }
